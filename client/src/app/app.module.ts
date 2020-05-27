341eb2ab1223ebb227e5eef7c4618e86898461dd
Carlos Ramos <cramosl3@miumg.edu.gt>
2020-05-27 01:08:22 -0600

    Code Refactoring
    The server location was changed to a global constant because it is more efficient to maintain.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,8 +8,9 @@ import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ProjectVariable } from './variables/projects.variables';
  
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: ProjectVariable.serverLocation, options: {} };
 
 
 @NgModule({
